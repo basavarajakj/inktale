f747030b640f149ee04b6bce0c18ebfc3772ff6e
Basavarajakjbasuu <basavarajakj06@gmail.com>
2024-08-17 13:46:05 +0530

    Established 🙏🙏session with MonogDB 🫂🫂

--- app.js
+++ app.js
@@ -10,11 +10,15 @@
  */
 const express = require('express');
 require('dotenv').config();
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
 
 /**
  * custom modules
  */
-const register = require('./src/routes/register_route.js')
+const register = require('./src/routes/register_route');
+const login = require('./src/routes/login_route');
 const { connectDB, disconnectDB } = require('./src/config/mongoose_config');
 
 /**
@@ -36,15 +40,43 @@ app.use(express.static(`${__dirname}/public`));
 /**
  * parse urlencoded body
  */
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 
 /**
- * Register page
+ * instances for session storage
+ */
+const store = new MongoStore({
+  mongoUrl: process.env.MANGO_CONNECTION_URI,
+  collectionName: 'session',
+  dbName: 'inktale'
+})
+
+/**
+ * initial express session
+ */
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store,
+  cookie: {
+    maxAge: Number(process.env.SESSION_MAX_AGE)
+  }
+}))
+
+
+/**
+ * register page
  */
 
-app.use('/register', register)
+app.use('/register', register);
 
+
+/**
+ * login page
+ */
+app.use('/login', login)
 
 /**
  * start server
---
+++ src/controllers/login_controller.js
@@ -0,0 +1,85 @@
+/**
+ * @license Apache-2.0
+ * @copyright 2024 Basavaraja K J
+ */
+
+'use strict';
+
+
+/**
+ * node module
+ */
+const bcrypt = require('bcrypt');
+
+
+/**
+ * custom modules
+ */
+const User = require('../models/user_model');
+
+
+/**
+ * Render the login page.
+ * 
+ * @param {object} req - The HTTP request object
+ * @param {object} res - The HTTP response object
+ */
+const renderLogin = (req, res) => {
+
+  res.render('./pages/login');
+}
+
+
+/**
+ * Handles the login precess for a user.
+ * 
+ * @param {object} req - The HTTP request object
+ * @param {object} res - The HTTP response object
+ * @returns {Promise<void>} - A promise representing the asynchronous operation.
+ */
+
+const postLogin = async (req, res) => {
+  try {
+    
+    // Extract email and password from request body
+    const { email, password } = req.body;
+
+    // Find user from database by email
+    const currentUser = await User.findOne({ email });
+
+    // Handle case user not found in database
+    if(!currentUser) {
+      return res.status(400).json({ message: 'Invalid credentials. Please ensure you\'ve entered the correct credentials and try again'})
+    }
+
+    // Check if password is valid
+    const passwordIsValid = await bcrypt.compare(password, currentUser.password);
+    
+    // Handle case where password is invalid
+    if(!passwordIsValid) {
+      return res.status(400).json({ message: 'Invalid credentials. Please ensure you\'ve entered the correct credentials and try again'});
+    }
+
+    // Set session userAUthenticated to true and redirect to homepage
+    req.session.user = {
+      userAuthenticated: true,
+      name: currentUser.name,
+      username: currentUser.username,
+      profilePhotoURL: currentUser.profilePhoto?.url
+    }
+
+    return res.redirect('/')
+    
+    
+  } catch (error) {
+    
+    console.log('postLogin: ', error.message);
+    
+
+  }
+}
+
+module.exports = {
+  renderLogin,
+  postLogin
+}
