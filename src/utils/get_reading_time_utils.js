d4a6d1682893c0d7f809424d333ede1c9b2f979a
Basavarajakjbasuu <basavarajakj06@gmail.com>
2024-08-24 14:19:54 +0530

    chore: Update font variation settings and reading time calculation

--- src/utils/get_reading_time_utils.js
+++ src/utils/get_reading_time_utils.js
@@ -5,7 +5,7 @@
 
 'use strict';
 
-const AVG_READ_WPM = 200;
+const AVG_READ_WPM = 238;
 const getReadingTime = (text) => Math.ceil(text.split('').length / AVG_READ_WPM);
 
 module.exports = getReadingTime;
